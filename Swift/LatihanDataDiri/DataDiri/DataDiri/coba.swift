efd5587e8c7f70f8c4605ab9267f96a29eb6f716
Dery Sudrajat <dery.sudrajat17@gmail.com>
2020-03-15 01:02:32 +0700

    Initial commit

---
+++ Swift/LatihanDataDiri/DataDiri/DataDiri/coba.swift
@@ -0,0 +1,9 @@
+//
+//  coba.swift
+//  DataDiri
+//
+//  Created by Lab. RPLM on 06/03/20.
+//  Copyright © 2020 Dery Sudrajat. All rights reserved.
+//
+
+import Foundation
---
+++ Swift/LatihanDataDiri/DataDiri/DataDiri/datadiri.swift
@@ -0,0 +1,9 @@
+//
+//  datadiri.swift
+//  DataDiri
+//
+//  Created by Lab. RPLM on 06/03/20.
+//  Copyright © 2020 Dery Sudrajat. All rights reserved.
+//
+
+import Foundation
